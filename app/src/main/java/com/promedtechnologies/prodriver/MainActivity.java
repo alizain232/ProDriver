7f8d240e6636e839186ce83d156000a7b107eb66
alizain232 <alizain03487100935@gmail.com>
2024-03-16 20:32:36 +0500

    Code

--- app/src/main/java/com/promedtechnologies/prodriver/MainActivity.java
+++ app/src/main/java/com/promedtechnologies/prodriver/MainActivity.java
@@ -2,7 +2,10 @@ package com.promedtechnologies.prodriver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        statusBarColor();
+
+    }
+    public void statusBarColor(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.system_ui)); // Set your color here
+        }
     }
 }
